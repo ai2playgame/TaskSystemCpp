afbf96967e32e9a2977d17e6635270127dac3355
ouchiminh <39828763+ouchiminh@users.noreply.github.com>
2019-02-17 20:37:09 +0900

    Create TaskFunction.hpp

---
+++ TaskSystem/aiUtil/task/TaskFunction.hpp
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <utility>
+#include <functional>
+#include <type_traits>
+
+namespace aiUtil::task {
+
+class TaskFunction {
+	std::function<void()> f_;
+public:
+	template<class T, std::enable_if_t<std::is_class_v<T>>* = nullptr>
+	TaskFunction(T* ptr, void(T::*func)())
+		: f_{ [ptr, func]() { ptr->*(*func)(); } }
+	{}
+	TaskFunction(std::function<void()> func)
+		: f_{std::move(func)}
+	{}
+
+	void operator()()
+	{
+		f_();
+	}
+	template<class T, std::enable_if_t<std::is_class_v<T>>* = nullptr>
+	void setNewFunction(T* ptr, void(T::*func)())
+	{
+		f_ = [ptr, func] {ptr->*(*func)(); };
+	}
+	void setNewFunction(const std::function<void()>& func)
+	{
+		f_ = func;
+	}
+	void setNewFunction(std::function<void()>&& func)
+	{
+		f_ = std::move(func);
+	}
+};
+
+}
